76233231f1875aad345924dba113b744e5b58a9f
Felice Forby <feliceforby@gmail.com>
2018-06-10 22:46:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  # attr_accessor :name, :type
+  # attr_reader :db
+  
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def find
+    
+  end
 end
